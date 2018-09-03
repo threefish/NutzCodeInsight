ac76c3399977b3e57c9f89478f69cc9501d47300
黄川 <306955302@qq.com>
2018-09-03 09:44:15 +0800

    一种新的解决办法，再尝试下其他办法

--- src/com/sgaop/project/module/NutzBootModuleBuilder.java
+++ src/com/sgaop/project/module/NutzBootModuleBuilder.java
@@ -5,10 +5,17 @@ import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.SettingsStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.module.ModifiableModuleModel;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 /**
  * @author 黄川 huchuc@vip.qq.com
@@ -36,6 +43,15 @@ public class NutzBootModuleBuilder extends JavaModuleBuilder {
     public ModuleWizardStep getCustomOptionsStep(WizardContext context, Disposable parentDisposable) {
         return new NutzBootMakerChooseStep(context);
     }
+
+    @Nullable
+    @Override
+    public List<Module> commit(@NotNull Project project, ModifiableModuleModel model, ModulesProvider modulesProvider) {
+        List<Module> moduleList = super.commit(project, model, modulesProvider);
+        System.out.println("我可以重新下载项目");
+        return moduleList;
+    }
+
 
     /**
      * 取得当前模块路径
